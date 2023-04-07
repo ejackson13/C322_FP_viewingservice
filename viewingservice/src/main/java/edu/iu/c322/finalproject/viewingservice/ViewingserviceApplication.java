10f78a4905e2b75ea432e729be97e50a889b41f0
Evan Jackson <evanjack@iu.edu>
2023-04-07 15:05:25 -0400

    Initial commit

---
+++ viewingservice/src/main/java/edu/iu/c322/finalproject/viewingservice/ViewingserviceApplication.java
@@ -0,0 +1,13 @@
+package edu.iu.c322.finalproject.viewingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViewingserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViewingserviceApplication.class, args);
+	}
+
+}
---
+++ viewingservice/src/test/java/edu/iu/c322/finalproject/viewingservice/ViewingserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.iu.c322.finalproject.viewingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViewingserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
